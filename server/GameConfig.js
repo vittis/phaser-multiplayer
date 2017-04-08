34ca41c0e8c09681d872a0b27b16c86336c9afa6
vittis <vitbolb1@gmail.com>
2017-04-08 19:29:28 -0300

    q

--- server/GameConfig.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var GameConfig;
-(function (GameConfig) {
-    GameConfig.PLAYER_MOVE = 4;
-})(GameConfig = exports.GameConfig || (exports.GameConfig = {}));
